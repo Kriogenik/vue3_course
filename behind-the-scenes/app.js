274663f2fde8229ca26b0c1a60a44c454a005e6d
Evgeniy <evgpashko92@gmail.com>
2021-01-20 16:10:37 +0700

    Vue Reactivity
    Deep dive in VueJS reactivity

--- behind-the-scenes/app.js
+++ behind-the-scenes/app.js
@@ -16,3 +16,33 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+
+/* Погружение в реактивность Vue */
+
+// Объект с некоторыми свойствами
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!'
+};
+
+// Объект-обработчик
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  }
+};
+
+// Объект Proxy
+const proxy = new Proxy(data, handler);
+
+// Результат до изменений
+console.log(proxy.longMessage);
+
+/* Новое значение для свойства message
+и вывод результата в консоль */
+proxy.message = 'Hello!!!!';
+console.log(proxy.longMessage);
