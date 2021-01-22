50602b7aead6af71e298edb4f60d2717f94ee0e3
Evgeniy <evgpashko92@gmail.com>
2021-01-22 14:10:20 +0700

    Create re-use Component

--- components-intro/app.js
+++ components-intro/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsIsVisible: false,
       friends: [
         {
           id: 'manuel',
@@ -16,6 +15,32 @@ const app = Vue.createApp({
           email: 'julie@localhost.com'
         }
       ]
+    };
+  }
+});
+
+/* Создаем компонент с именем friend-contact. В HTML
+подключение происходит по тегу friend-contact */
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name}}</h2>
+    <button @click="toggleDetails">{{ detailsIsVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsIsVisible">
+      <li><strong>Phone:</strong> {{ friend.phone}}</li>
+      <li><strong>Email:</strong> {{ friend.email}}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsIsVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com'
+      }
     };
   },
   methods: {
