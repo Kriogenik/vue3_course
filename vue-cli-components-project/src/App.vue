3c287e9a5c01fe16b0646b5bfffa3cf3c112eb41
Evgeniy <evgpashko92@gmail.com>
2021-01-24 17:22:56 +0700

    Add some information
    Add some info to data instance

--- vue-cli-components-project/src/App.vue
+++ vue-cli-components-project/src/App.vue
@@ -1,11 +1,31 @@
 <template>
-	<h2>Application Name</h2>
+	<section>
+		<h2>My fiends</h2>
+		<ul>
+			<li></li>
+		</ul>
+	</section>
 </template>
 
 <script>
 export default {
 	data() {
-		return {}
+		return {
+			friends: [
+				{
+					id: 'viktor',
+					name: 'Viktor Ivanov',
+					phone: '8987 264 2354',
+					email: 'viktor@localhost.com'
+				},
+				{
+					id: 'maxim',
+					name: 'Maxim Petrov',
+					phone: '8941 846 1234',
+					email: 'maxim@localhost.com'
+				}
+			]
+		}
 	}
 };
 </script>
