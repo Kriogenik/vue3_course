1d60228485e9e536915cd63fe3fdc23024c71e66
Evgeniy <evgpashko92@gmail.com>
2021-02-13 14:38:36 +0700

    Create vetur.config.js

---
+++ vetur.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	setting: {
+		"vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true
+	},
+	projects: [
+		{
+			root: './',
+			package: './cmp-adv/package.json'
+		}
+	]
+}
