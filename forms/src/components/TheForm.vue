31fe796ea99edbaed513c3b976bd00417b341a76
Evgeniy <evgpashko92@gmail.com>
2021-03-06 19:56:43 +0700

    Using v-model with Checkboxes & Radiobuttons
    With Checkboxes have bug (check all)

--- forms/src/components/TheForm.vue
+++ forms/src/components/TheForm.vue
@@ -19,30 +19,30 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" v-model="interest" />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" v-model="interest" />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" v-model="interest" />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" v-model="how" />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" v-model="how" />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" v-model="how" />
         <label for="how-other">Other</label>
       </div>
     </div>
@@ -58,7 +58,9 @@ export default {
     return {
       userName: '',
       userAge: null,
-      referrer: 'wom'
+      referrer: 'wom',
+      interest: null,
+      how: null
     }
   },
   methods: {
@@ -75,6 +77,12 @@ export default {
       // Dropdown
       console.log('Referrer: ' + this.referrer);
       this.referrer = '';
+      //Checkboxes
+      console.log('Checkboxes: ' + this.interest);
+      this.interest = null;
+      // Radio
+      console.log('Radio buttons: ' + this.how);
+      this.how = null;
     }
   }
 }
