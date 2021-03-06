3588f579d1e48c9e9e1294c13782a5798c7bb273
Evgeniy <evgpashko92@gmail.com>
2021-03-06 23:19:03 +0700

    Update RatingControl.vue
    Adding active styles and some logic

--- forms/src/components/RatingControl.vue
+++ forms/src/components/RatingControl.vue
@@ -1,32 +1,61 @@
 <template>
-	<ul>
-		<li><button type="button">Poor</button></li>
-		<li><button type="button">Average</button></li>
-		<li><button type="button">Great</button></li>
-	</ul>
+  <ul>
+    <li :class="{active: activeOption === 'poor'}">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{active: activeOption === 'average'}">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{active: activeOption === 'great'}">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      activeOption: null,
+    }
+  },
+  methods: {
+    activate(option) {
+      this.activeOption = option;
+    }
+  }
+};
+</script>
+
 <style scoped>
+.active {
+  border-color: #00a3a3;
+}
+
+.active button {
+  color: #00a3a3;
+}
+
 ul {
-	display: flex;
-	margin: 0.5rem 0;
-	padding: 0;
-	list-style: none;
+  display: flex;
+  margin: 0.5rem 0;
+  padding: 0;
+  list-style: none;
 }
 
 li {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin: 0 1rem;
-	padding: 1rem;
-	border: 1px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 1rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
 }
 
 button {
-	font: inherit;
-	border: none;
-	background-color: transparent;
-	cursor: pointer;
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
 }
 </style>
